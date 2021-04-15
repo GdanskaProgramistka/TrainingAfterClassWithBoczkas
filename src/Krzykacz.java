69b4b8cbfb5f411d5a5dc852c1f09f80779d9083
GdanskaProgramistka <emug@vp.pl>
2021-04-15 20:46:54 +0200

    Use of lambda

---
+++ src/Krzykacz.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Krzykacz {
+    void wydajDzwiek();
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+    //WSZYSTKIE ZADANIA WYKONAć PRZY UŻYCIU LAMBDY:
+
+    // Krzykacz kuchenny:
+        Krzykacz kuchennyKrzykacz = () -> System.out.println("Umyj gary Dziadu!");
+        kuchennyKrzykacz.wydajDzwiek();
+
+    //Krzykacz ma krzyknać wielokrotnie:
+        Krzykacz krzykaczWielokrotny = () -> {
+            for (int i = 0; i < 4; i++) {
+                System.out.println("Krzyczę po raz: " + i);
+            }
+        };
+        krzykaczWielokrotny.wydajDzwiek();
+
+    //Manager ma podać komunikat o podwyżce:
+        Manager milymanager = podwyzka -> System.out.println("Daję Ci podwyżkę w wysokości " + podwyzka);
+        Manager niemilymanager = podwyzka -> System.out.println("Nic nie dostaniesz Ty głupi Dziadu! Jeszcze obniżę Ci pesnję o: " + podwyzka);
+
+        milymanager.podajInfoOPodwyzce(100);
+        niemilymanager.podajInfoOPodwyzce(-10);
+    }
+}
