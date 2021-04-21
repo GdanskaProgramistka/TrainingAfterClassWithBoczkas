683bdd172d75d43a6fdac5a96cda818565832617
GdanskaProgramistka <emug@vp.pl>
2021-04-21 19:13:33 +0200

    Enum example + methods

--- src/EnumExample/Main.java
+++ src/EnumExample/Main.java
@@ -1,8 +1,11 @@
 package EnumExample;
 
 import java.time.DayOfWeek;
+import java.util.Arrays;
 
 public class Main {
+
+
     public static void main(String[] args) {
 
         DayOfTheWeek today = DayOfTheWeek.SUNDAY;
@@ -14,5 +17,26 @@ public class Main {
         else {
             System.out.println("Today is not Sunday");
         }
+        // metoda valueOff:
+        DayOfTheWeek secondDay = Enum.valueOf(DayOfTheWeek.class, "TUESDAY");
+        System.out.println(secondDay);
+
+        // metoda toString():
+        System.out.println(DayOfTheWeek.MONDAY.toString());
+
+        // metoda value(Class enumClass, String name):
+        DayOfTheWeek[] values = DayOfTheWeek.values();
+        System.out.println(Arrays.toString(values));
+
+        // metoda odinal (zwracająca położenie):
+        System.out.println(DayOfTheWeek.FRIDAY.ordinal());
+
+        // metoda compareTo:
+        System.out.println(DayOfTheWeek.MONDAY.compareTo(DayOfTheWeek.FRIDAY));
+        System.out.println(DayOfTheWeek.FRIDAY.compareTo(DayOfTheWeek.MONDAY));
+
+
     }
 }
+
+
